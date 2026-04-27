9cb26db3e9cd74c09447f1fd8cf3b6c8bcd0b706
Rahul Singh <rahul274301@gmail.com>
2026-04-27 18:59:57 +0530

    fix: Refactor CORS origin handling for improved clarity and security

--- src/app.js
+++ src/app.js
@@ -6,33 +6,43 @@ import session from "express-session";
 const app = express();
 
 app.set("trust proxy", 1);
-const allowedOrigins = [
-  process.env.CORS_ORIGIN,
+
+const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(",").map(o => o.trim())
+  : [];
+
+const devOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "http://localhost:3005",
-  "http://localhost:3006",
-  "http://localhost:5173",
+  "http://localhost:3002",
   "http://127.0.0.1:3000",
   "http://127.0.0.1:3001",
-  "http://192.168.1.8:3000",
+  "http://192.168.1.5:3000", // Adjust this to your local IP(Router) and port
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps or curl requests)
+      // allow non-browser requests
       if (!origin) return callback(null, true);
+
+      // allow exact matches
       if (
-        allowedOrigins.indexOf(origin) !== -1 ||
-        process.env.CORS_ORIGIN === "*"
+        allowedOrigins.includes(origin) ||
+        devOrigins.includes(origin)
       ) {
         return callback(null, true);
-      } else {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
       }
+
+      // allow  root and all subdomains of syntx.in
+      if ( origin === "https://syntx.in" || origin.endsWith(".syntx.in")) {
+        return callback(null, true);
+      }
+
+      return callback(
+        new Error("Not allowed by CORS"),
+        false
+      );
     },
     credentials: true, // Important for sending cookies with cross-origin requests
   }),
